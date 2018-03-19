7c9b72a793bc77285e4d1e5c651cdb53536b455c
ash <aichiko66@163.com>
2018-03-19 09:19:25 +0800

    Initial commit

---
+++ CCEmptyDataSet/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  CCEmptyDataSet
+//
+//  Created by 赵光飞 on 2018/3/19.
+//  Copyright © 2018年 赵光飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UITableViewController
+
+@end
---
+++ CCEmptyDataSet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCEmptyDataSet
+//
+//  Created by 赵光飞 on 2018/3/19.
+//  Copyright © 2018年 赵光飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
